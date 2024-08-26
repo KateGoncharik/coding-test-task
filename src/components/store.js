8138e3dd8110f097254817eeb6801263578c0b0f
Kate Goncharick <113840749+KateGoncharik@users.noreply.github.com>
2024-08-26 17:22:01 +0300

    fix: remove comments

--- src/components/store.js
+++ src/components/store.js
@@ -6,18 +6,3 @@ export const getPosts = async () => {
     throw new Error('Error while getting posts');
   }
 };
-
-// export async function getProducts(offset = 0) {
-//   const queryArgs = {
-//     limit: productsPerPage,
-//     offset,
-//   };
-//   try {
-//     const request = await getApiRoot().productProjections().get({ queryArgs }).execute();
-//     const products = request.body;
-//     return products;
-//   } catch (err) {
-//     console.error(errorMessage);
-//     return null;
-//   }
-// }
